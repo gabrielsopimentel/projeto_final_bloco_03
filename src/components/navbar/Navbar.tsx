e44f330998bd6948d1ae77d07f1b2a18f67f2b48
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-11-09 10:25:56 -0300

    home navbar footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                Projeto Final Bloco 3
+
+                    <div className='flex gap-4'>
+                        Categorias
+                        Cadastrar 
+                        Atualizar 
+                        Deletar 
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
