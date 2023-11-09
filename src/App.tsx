cc1d336f774bc65a8c1bc4896cebeafa2d6dd444
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-11-09 11:51:08 -0300

    crud cat

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar"
-import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import ListaCategorias from "./components/categorias/listaCategorias/ListaCategorias";
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-      <Home />
-      <Footer />
-
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<ListaCategorias />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
